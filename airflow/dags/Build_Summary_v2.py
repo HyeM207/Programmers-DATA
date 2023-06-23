4bbd33a856c859b37ff3ef8a398e3d52e5165483
HyeM207 <hmk9667@gmail.com>
2023-06-23 16:52:53 +0900

    feat: 230619일자 Airflow 실습

---
+++ airflow/dags/Build_Summary_v2.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+from airflow.macros import *
+
+import os
+from glob import glob
+import logging
+import subprocess
+
+from plugins import redshift_summary
+from plugins import slack
+
+"""
+실습 : config 파일로 따로 빼고, plugins의 함수를 이용하여 nps_summary 테이블 만들기
+"""
+
+DAG_ID = "Build_Summary_v2"
+dag = DAG(
+    DAG_ID,
+    schedule_interval="25 13 * * *",
+    max_active_runs=1,
+    concurrency=1,
+    catchup=False,
+    start_date=datetime(2021, 9, 17),
+    default_args= {
+        'on_failure_callback': slack.on_failure_callback,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=1),
+    }
+)
+
+# this should be listed in dependency order (all in analytics)
+tables_load = [
+    'nps_summary'
+]
+
+dag_root_path = os.path.dirname(os.path.abspath(__file__))
+redshift_summary.build_summary_table(dag_root_path, dag, tables_load, "redshift_dev_db")
